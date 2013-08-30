9e39b2efde9d8d9466f89372ccbb1f8936e381d3
Conor <conoreflanagan@gmail.com>
2013-08-30 13:53:48 -0700

    Refactor and edited admin contact page to show all icons

---
+++ helpers.rb
@@ -0,0 +1,5 @@
+helpers do
+	def hellodur 
+		puts "*********************HELLO"
+	end
+end
--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/namespace'
 require 'data_mapper'
+require_relative 'helpers.rb'
 enable :sessions
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
@@ -24,7 +25,7 @@ class Video
 end
 DataMapper.finalize.auto_upgrade!
 
-get '/' do 
+get '/?' do 
 	erb :index
 end
 
@@ -33,26 +34,30 @@ end
 # 		erb route.to_sym
 # 	end
 # end
-get '/login' do
-	erb :login
-end
 
 get '/projects' do 
+	hellodur
 	@projects = Video.all
 	@tags = []
 	@tags = @projects.collect { |proj| proj.tags.split(', ')}.flatten.uniq
 	erb :projects
 end
 
-['/about', '/resume', '/contact'].each do |route|
+['/about', '/resume'].each do |route|
 	get route do 
 		@article = Article.first(title: route[1,10].capitalize)
 		erb :show
 	end
 end
 
+['/login', '/contact'].each do |route|
+	get route do 
+		erb route.to_sym
+	end
+end
+
 post '/login' do
-	puts params.inspect
+	#login helper
 	if(params['username'] == 'walterwhite' && params['password'] == 'heisenberg')
 		session['loggedIn'] = true
 		redirect '/admin'
@@ -66,17 +71,9 @@ get '/logout' do
 	redirect '/'
 end
 
-# get '/login' do
-# 	erb :login
-# end
-
-# get '/projects' do
-# 	erb :projects
-# end
-
 namespace '/admin' do
 	before { redirect '/login' unless session['loggedIn']}
-	['/welcome/edit', '/about/edit', '/resume/edit', '/contact/edit'].each do |route|
+	['/welcome/edit', '/about/edit', '/resume/edit'].each do |route|
 		get route do 
 			@article = Article.first(title: route[1,route.index('/edit')-1].capitalize)
 			erb :'/admin/editarticle'
@@ -88,67 +85,53 @@ namespace '/admin' do
 		end
 	end
 	#IS THIS THE RIGHT WAY TO DO THIS?
-	get '' do
+	get '/?' do
 		erb :'admin/index'
 	end
-
-#PUT ALL THIS IN A PROJECTS NAMESPACE also find where to change href="<%= .. %>"
-get '/projects/:id/edit' do
-	@project = Video.get(params[:id])
-	erb :'admin/edit'
-end
-get '/projects' do
-	@projects = Video.all
-	erb :'admin/adminprojects'
-end
-get '/projects/new' do
-	erb :'admin/new'
-end
-post '/projects/new' do
-	Video.create(
-		title: params[:title], 
-		url: params[:url], 
+	get '/contact/edit' do
+		@icons = Dir.entries('public/img/contact_icons') - [".",".."]
+		erb :'admin/contact'
+	end
+	#PUT ALL THIS IN A PROJECTS NAMESPACE also find where to change href="<%= .. %>"
+	get '/projects/:id/edit' do
+		@project = Video.get(params[:id])
+		erb :'admin/edit'
+	end
+	get '/projects' do
+		@projects = Video.all
+		erb :'admin/adminprojects'
+	end
+	get '/projects/new' do
+		erb :'admin/new'
+	end
+	post '/projects/new' do
+		Video.create(
+			title: params[:title], 
+			url: params[:url], 
 			#move to helper
 			img_url: "http://img.youtube.com/vi/"+params[:url][/(watch\?v=)(.{11})/,2]+"/0.jpg",
 			description: params[:description],
 			tags: params[:tags]
 			)
-	redirect '/projects'
-end
-put '/projects/:id' do
-	Video.get(params[:id]).update(
-		title: params[:title], 
-		url: params[:url], 
+		redirect '/projects'
+	end
+	put '/projects/:id' do
+		Video.get(params[:id]).update(
+			title: params[:title], 
+			url: params[:url], 
 		#move to helper
 		img_url: "http://img.youtube.com/vi/"+params[:url][/(watch\?v=)(.{11})/,2]+"/0.jpg",
 		description: params[:description],
 		tags: params[:tags]
 		)
-	redirect '/admin/projects'
+		redirect '/admin/projects'
+	end
+
+	delete '/projects/:id' do
+		Video.get(params[:id]).destroy
+		redirect '/admin/projects'
+	end
 end
-
-delete '/projects/:id' do
-	Video.get(params[:id]).destroy
-	redirect '/admin/projects'
-end
-end
-
-	# get '/admin' do
-	# 	erb :'admin/index'
-	# end
-
-# get '/admin/video/new' do
-# 	erb :new
-# end
-
-# get '/admin/video/edit' do
-# 	erb :edit
-# end
-# get '/otherstuff' do
-# 	text = File.open("otherstuff.txt").gets
-# 	text
-# end
-#admin routes
 
 #404
 not_found do
