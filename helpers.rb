1179647d66b90936634906a66752136ca6dda13b
Conor <conoreflanagan@gmail.com>
2013-09-01 13:25:42 -0700

    Routes refactored with helpers

--- helpers.rb
+++ helpers.rb
@@ -1,5 +1,37 @@
 helpers do
-	def hellodur 
-		puts "*********************HELLO"
+	def login!
+		session[:loggedIn] = true
+	end
+
+	def logout!
+		session.clear
+		redirect '/'
+	end
+
+	def logged_in?
+		session[:loggedIn]
+	end
+
+	def url_to_img_url url
+		"http://img.youtube.com/vi/"+url[/(watch\?v=)(.{11})/,2]+"/0.jpg"
+	end
+
+	def authenticate!
+		if(params['username'] == 'walterwhite' && params['password'] == 'heisenberg')
+			login!
+			redirect '/admin'
+		else
+			redirect '/login'
+		end
+	end
+	def to_title route
+		route[1,route.index('/edit')-1].capitalize
+	end
+	# Removes _method param that was added to form to make put/delete work.
+	def params_fixer params
+		#where does :captures come from??
+		fixed_params = params.select { |key, value| key != '_method' && key != 'splat' && key != 'captures' }
+		fixed_params['img_url'] = url_to_img_url(params[:url])
+		fixed_params
 	end
 end
--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/namespace'
 require 'data_mapper'
-require_relative 'helpers.rb' #this file is blank...
+require_relative 'helpers.rb' 
 enable :sessions
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
@@ -32,122 +32,48 @@ class Contact
 end
 DataMapper.finalize.auto_upgrade!
 
-get '/?' do 
-	erb :index
-end
-#
-# ['/login', '/projects'].each do |route|
-# 	get route do
-# 		erb route.to_sym
-# 	end
-# end
-
+#Public Routes
+get('/?') { erb :index }
 get '/projects' do 
-	hellodur
 	@projects = Video.all
 	@tags = []
 	@tags = @projects.collect { |proj| proj.tags.split(', ')}.flatten.uniq
 	erb :projects
 end
-
 ['/about', '/resume'].each do |route|
 	get route do 
 		@article = Article.first(title: route[1,10].capitalize)
 		erb :show
 	end
 end
-
-# ['/login', '/contact'].each do |route|
-# 	get route do 
-# 		erb route.to_sym
-# 	end
-# end
-get '/login' do
-	erb :login
-end
-
 get '/contacts' do
 	@my_contacts = Contact.all
-	puts "*"*20
-	puts @my_contacts
 	erb :contacts
 end
 
-post '/login' do
-	#login helper
-	if(params['username'] == 'walterwhite' && params['password'] == 'heisenberg')
-		session['loggedIn'] = true
-		redirect '/admin'
-	else
-		redirect '/login'
-	end
-end
+#Login Routes
+get('/login') { erb :login }
+get('/logout') { logout! }
+post('/login') { authenticate! }
 
-get '/logout' do
-	session.clear
-	redirect '/'
-end
-
+#/admin Routes/Namespace
 namespace '/admin' do
-	before { redirect '/login' unless session['loggedIn']}
+	before { redirect '/login' unless logged_in? }
+	get('/?') { erb :'admin/index' }
 	['/welcome/edit', '/about/edit', '/resume/edit'].each do |route|
 		get route do 
-			@article = Article.first(title: route[1,route.index('/edit')-1].capitalize)
+			@article = Article.first(title: to_title(route))
 			erb :'/admin/editarticle'
 		end
 		post route do
-			@article = Article.first(title: route[1,route.index('/edit')-1].capitalize)
-			@article.update(content: params[:content])
+			@article = Article.first(title: to_title(route)).update(content: params[:content])
 			redirect '/admin'
 		end
-	end
-	#IS THIS THE RIGHT WAY TO DO THIS?
-	get '/?' do
-		erb :'admin/index'
 	end
 	get '/contacts' do
 		@my_contacts = Contact.all
 		@icons = Dir.entries('public/img/contact_icons') - [".",".."]
 		erb :'admin/contacts'
-	end
-	#PUT ALL THIS IN A PROJECTS NAMESPACE also find where to change href="<%= .. %>"
-	get '/projects/:id/edit' do
-		@project = Video.get(params[:id])
-		erb :'admin/edit'
-	end
-	get '/projects' do
-		@projects = Video.all
-		erb :'admin/adminprojects'
-	end
-	get '/projects/new' do
-		erb :'admin/new'
-	end
-	post '/projects/new' do
-		Video.create(
-			title: params[:title], 
-			url: params[:url], 
-			#move to helper
-			img_url: "http://img.youtube.com/vi/"+params[:url][/(watch\?v=)(.{11})/,2]+"/0.jpg",
-			description: params[:description],
-			tags: params[:tags]
-			)
-		redirect '/projects'
-	end
-	put '/projects/:id' do
-		Video.get(params[:id]).update(
-			title: params[:title], 
-			url: params[:url], 
-		#move to helper
-		img_url: "http://img.youtube.com/vi/"+params[:url][/(watch\?v=)(.{11})/,2]+"/0.jpg",
-		description: params[:description],
-		tags: params[:tags]
-		)
-		redirect '/admin/projects'
-	end
-
-	delete '/projects/:id' do
-		Video.get(params[:id]).destroy
-		redirect '/admin/projects'
 	end
 	get '/contacts/:type/new' do
 		@type = params[:type]+"-48-black.png"
@@ -160,6 +86,34 @@ namespace '/admin' do
 	delete '/contacts/:id' do
 		Contact.get(params[:id]).destroy
 		redirect '/admin/contacts'
+	end
+	# /admin/projects Routes/Namespace
+	namespace '/projects' do
+		get '/?' do
+			@projects = Video.all
+			erb :'admin/adminprojects'
+		end
+		get('/new') { erb :'admin/new' }
+		post '/new' do
+			#NEED VALIDATATIONS
+			puts params.inspect
+			Video.create(params_fixer(params))
+			redirect '/projects'
+		end
+		get '/:id/edit' do
+			@project = Video.get(params[:id])
+			puts @project.inspect
+			erb :'admin/edit'
+		end
+		put '/:id/edit' do
+			puts params.inspect
+			Video.get(params[:id]).update(params_fixer(params))
+			redirect '/admin/projects'
+		end
+		delete '/:id' do
+			Video.get(params[:id]).destroy
+			redirect '/admin/projects'
+		end
 	end
 end
 
